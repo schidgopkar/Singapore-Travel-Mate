7276fe498231497ab0fa64a43a5c6dfe2f9df3a5
shrikantchidgopkar <shri.c@hotmail.com>
2020-07-11 21:40:12 +0800

    Adding Google Maps SDK to the project

--- Singapore Travel Mate/AppDelegate.m
+++ Singapore Travel Mate/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+@import GoogleMaps;
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    
     return YES;
 }
 
